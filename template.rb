1321bc2a0da17d0de8a3d3cc0a3649a52954eed1
Mathew Trivett <mathew@wearesnook.com>
2020-10-08 22:29:23 +0100

    feat: Documentation generation
    Created and manually tested the ability to generate docs folder as described in #6 and #7

--- template.rb
+++ template.rb
@@ -2,4 +2,22 @@
 #
 #
 def source_paths
+  [__dir__]
+end
+
+def set_app_name
+end
+
+# Docs
+# ~~~~~~~~~~~
+# 
+
+def create_docs
+  directory "docs"
+end
+
+source_paths
+
+after_bundle do
+  create_docs
 end
