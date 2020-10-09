d9c0e91227cc8582305580769154fa2d6065eac9
Mathew Trivett <mathew@wearesnook.com>
2020-10-09 09:28:06 +0100

    Added RDoc doc strings to outline suggested method naming convention
    To maintain consistency within this project template but also other Rails templates at https://github.com/wearesnook, I propose using a naming convention for template methods.

--- template.rb
+++ template.rb
@@ -1,3 +1,25 @@
+##
+# == Naming template methods
+# We propose to use a naming convention for template methods to 
+# create consistency between this and other Rails templates 
+# at https://github.com/WeAreSnook.
+#
+# This documentation uses [RDoc Markup](https://ruby-doc.org/stdlib-2.5.1/libdoc/rdoc/rdoc/RDoc/Markup.html)
+#
+# === Principles
+#
+# * Use a verb_ prefix to describe what the method does
+# * Use simple language to describe the action
+# 
+# === Suggested prefixes
+# 
+# [add_]  add dependencies
+# [create_] create files or directories
+# [destroy_] remove files or directories 
+# [config_] write config files / variables
+# [install_] install third party tools
+# [generate_] run rails generators
+
 # source_paths
 #
 #
@@ -5,13 +27,13 @@ def source_paths
   [__dir__]
 end
 
-def set_app_name
+def config_app_name
 end
 
-# Docs
-# ~~~~~~~~~~~
-# 
 
+##
+# Copies the docs directory
+#
 def create_docs
   directory "docs"
 end
