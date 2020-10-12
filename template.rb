02987cdd96a6c7a0e5a4ef602aa2313d8981dff9
Mathew Trivett <mathew@wearesnook.com>
2020-10-12 15:07:38 +0100

    docs: Added templates for docs and project docs
    GDS recommend having a `docs` directory in every rails project to store documentation.
    The template automatically creates this docs directory for you.
    
    > See #6 https://github.com/WeAreSnook/govuk_rails_app_template/issues/6
    
    GDS recommend using architectural decision records to log technical decisions on a project.
    
    > See #7 https://github.com/WeAreSnook/govuk_rails_app_template/issues/7
    
    Builds a sample README for the project with instructions on how to get started.
    
    > See #10 https://github.com/WeAreSnook/govuk_rails_app_template/issues/10

--- template.rb
+++ template.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 ##
 # == Naming template methods
-# We propose to use a naming convention for template methods to 
-# create consistency between this and other Rails templates 
+# We propose to use a naming convention for template methods to
+# create consistency between this and other Rails templates
 # at https://github.com/WeAreSnook.
 #
 # This documentation uses [RDoc Markup](https://ruby-doc.org/stdlib-2.5.1/libdoc/rdoc/rdoc/RDoc/Markup.html)
@@ -10,12 +12,12 @@
 #
 # * Use a verb_ prefix to describe what the method does
 # * Use simple language to describe the action
-# 
+#
 # === Suggested prefixes
-# 
+#
 # [add_]  add dependencies
 # [create_] create files or directories
-# [destroy_] remove files or directories 
+# [destroy_] remove files or directories
 # [config_] write config files / variables
 # [install_] install third party tools
 # [generate_] run rails generators
@@ -23,23 +25,30 @@
 # source_paths
 #
 #
+
 def source_paths
   [__dir__]
 end
-
-def config_app_name
-end
-
 
 ##
 # Copies the docs directory
 #
 def create_docs
-  directory "docs"
+  directory 'templates/docs', 'docs'
+end
+
+def create_readme
+  template 'templates/README.md.tt', 'README.md'
+end
+
+def create_contributing
+  template 'templates/CONTRIBUTING.md.tt', 'CONTRIBUTING.md'
 end
 
 source_paths
 
 after_bundle do
   create_docs
+  create_readme
+  create_contributing
 end
